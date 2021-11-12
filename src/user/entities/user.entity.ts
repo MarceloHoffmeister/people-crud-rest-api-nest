f5855e630a3714f5c6023e4681a5a3f75d86ce11
Marcelo Hoffmeister <marcelohenriquehoffmeister@gmail.com>
2021-11-12 03:03:39 -0400

    feat: fill user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false })
+  username: string;
+
+  @Column({ nullable: false })
+  email: string;
+
+  @Column({ nullable: false })
+  password: string;
+}
