70e39d8227c251ec5d7944b099ad1c46ec90f631
Marcelo Hoffmeister <marcelohenriquehoffmeister@gmail.com>
2021-11-14 01:50:19 -0400

    chore: create orm configuration

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,10 @@
 module.exports = {
-  type: 'postgres',
-  url: process.env.DB_URL,
+  type: process.env.DB_DRIVER,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [process.env.ENTITY_PATH],
   synchronize: true,
 };
