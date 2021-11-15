7649d71399c502b01b592c7c5b0be955d6831c5e
Marcelo Hoffmeister <marcelohenriquehoffmeister@gmail.com>
2021-11-15 00:17:57 -0400

    feat: generated auth module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserModule],
+  imports: [TypeOrmModule.forRoot(), UserModule, AuthModule],
 })
 export class AppModule {}
